85444b2d078038aed302aa03f988d703cef10e40
Aaron Fulwider <icestorm0806@yahoo.com>
2020-12-18 02:35:45 -0800

    no message

--- Pet Records/View/Pets/BasicsViewController.swift
+++ Pet Records/View/Pets/BasicsViewController.swift
@@ -16,7 +16,6 @@ class BasicsViewController: UIViewController {
     let introLabel = UILabel()
     
     let vertStackView = UIStackView()
-    // textViews
     let medTextView = UITextView() // names and doses
     let foodTextView = UITextView() // food they eat
     let habitsTextView = UITextView() // eating and drinking habits
@@ -25,7 +24,6 @@ class BasicsViewController: UIViewController {
     let historyTextView = UITextView() // medical history
     
     func fetchBasics() {
-        
         let uid = Auth.auth().currentUser?.uid
         Database.database().reference().child("users").child(uid!).child("pets").child((pet?.pid)!).child("Basics").observe(.value) { (snapshot) in
             if let dictionary = snapshot.value as? [String:AnyObject]{
@@ -44,13 +42,13 @@ class BasicsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editTapped))
         setupUI()
         fetchBasics()
     }
     
     @objc func editTapped() {
-        
         if vertStackView.isUserInteractionEnabled {
             navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editTapped))
             vertStackView.isUserInteractionEnabled = false
@@ -61,25 +59,15 @@ class BasicsViewController: UIViewController {
     }
     
     func setupUI() {
+        let toolBar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let submit  = UIBarButtonItem(title: "DONE", style: .done, target: self, action: #selector(doneButtonTapped))
+        let items = [flexSpace, submit]
+        toolBar.items = items
+        toolBar.sizeToFit()
+        
         view.addSubview(introLabel)
-        view.addSubview(vertStackView)
-        
-        vertStackView.addArrangedSubview(medTextView)
-        vertStackView.addArrangedSubview(foodTextView)
-        vertStackView.addArrangedSubview(habitsTextView)
-        vertStackView.addArrangedSubview(toiletTextView)
-        vertStackView.addArrangedSubview(bitesTextView)
-        vertStackView.addArrangedSubview(historyTextView)
-        
         introLabel.translatesAutoresizingMaskIntoConstraints = false
-        medTextView.translatesAutoresizingMaskIntoConstraints = false
-        foodTextView.translatesAutoresizingMaskIntoConstraints = false
-        habitsTextView.translatesAutoresizingMaskIntoConstraints = false
-        toiletTextView.translatesAutoresizingMaskIntoConstraints = false
-        bitesTextView.translatesAutoresizingMaskIntoConstraints = false
-        historyTextView.translatesAutoresizingMaskIntoConstraints = false
-        vertStackView.translatesAutoresizingMaskIntoConstraints = false
-        
         introLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
         introLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5).isActive = true
         introLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5).isActive = true
@@ -90,66 +78,75 @@ class BasicsViewController: UIViewController {
         introLabel.font = UIFont(name: "Arial-BoldMT", size: 40)
         introLabel.text = "This is where you will enter your pet's basic information.\n(Questions your vet will likely ask you on a first visit)"
         
+        view.addSubview(vertStackView)
+        vertStackView.translatesAutoresizingMaskIntoConstraints = false
         vertStackView.topAnchor.constraint(equalTo: introLabel.bottomAnchor).isActive = true
         vertStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         vertStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         vertStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         vertStackView.distribution = .fillEqually
-        
         vertStackView.axis = .vertical
         vertStackView.spacing = 4
+        vertStackView.isUserInteractionEnabled = false
+        
+        vertStackView.addArrangedSubview(medTextView)
+        medTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        vertStackView.addArrangedSubview(foodTextView)
+        foodTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        vertStackView.addArrangedSubview(habitsTextView)
+        habitsTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        vertStackView.addArrangedSubview(toiletTextView)
+        toiletTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        vertStackView.addArrangedSubview(bitesTextView)
+        bitesTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        vertStackView.addArrangedSubview(historyTextView)
+        historyTextView.translatesAutoresizingMaskIntoConstraints = false
         
         medTextView.layer.borderWidth = 1
         medTextView.layer.borderColor = UIColor.black.cgColor
         medTextView.text = "Medication"
+        medTextView.inputAccessoryView = toolBar
         
         foodTextView.layer.borderWidth = 1
         foodTextView.layer.borderColor = UIColor.black.cgColor
         foodTextView.text = "Food"
+        foodTextView.inputAccessoryView = toolBar
         
         habitsTextView.layer.borderWidth = 1
         habitsTextView.layer.borderColor = UIColor.black.cgColor
         habitsTextView.text = "Eating Habits"
+        habitsTextView.inputAccessoryView = toolBar
         
         toiletTextView.layer.borderWidth = 1
         toiletTextView.layer.borderColor = UIColor.black.cgColor
         toiletTextView.text = "Toilet Habits"
+        toiletTextView.inputAccessoryView = toolBar
         
         bitesTextView.layer.borderWidth = 1
         bitesTextView.layer.borderColor = UIColor.black.cgColor
         bitesTextView.text = "Recent tick/flea bites?"
+        bitesTextView.inputAccessoryView = toolBar
         
         historyTextView.layer.borderWidth = 1
         historyTextView.layer.borderColor = UIColor.black.cgColor
         historyTextView.text = "Vaccination History"
-        
-        let toolBar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
-        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-        let submit  = UIBarButtonItem(title: "DONE", style: .done, target: self, action: #selector(doneButtonTapped))
-        let items = [flexSpace, submit]
-        toolBar.items = items
-        toolBar.sizeToFit()
-        
-        medTextView.inputAccessoryView = toolBar
-        
-        foodTextView.inputAccessoryView = toolBar
-        habitsTextView.inputAccessoryView = toolBar
-        toiletTextView.inputAccessoryView = toolBar
-        bitesTextView.inputAccessoryView = toolBar
         historyTextView.inputAccessoryView = toolBar
-        vertStackView.isUserInteractionEnabled = false
     }
     
     @objc func saveEdit() {
         let uid = Auth.auth().currentUser?.uid
-        
         let medText = medTextView.text
         let foodText = foodTextView.text
         let habitText = habitsTextView.text
         let toiletText = toiletTextView.text
         let biteText = bitesTextView.text
         let historyText = historyTextView.text
-        
+         
         let ref = Database.database().reference().child("users").child(uid!).child("pets").child((pet?.pid)!).child("Basics")
         let values = ["medication": medText, "food": foodText, "eating": habitText, "toilet": toiletText, "bites": biteText, "history": historyText]
         DispatchQueue.main.async(execute: { ref.updateChildValues(values as [String:AnyObject]) })
